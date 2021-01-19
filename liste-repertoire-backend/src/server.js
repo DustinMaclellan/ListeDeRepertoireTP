00ddfc9fa9470ebb406cddec6d9b02a69e8c01f3
Dustin <dustin.maclellan@hotmail.com>
2021-01-19 14:53:35 -0500

    Revert "Revert "creation du projet""
    This reverts commit 82ae869a955a6fd52bd02ce3cea5523c8fc87b9b.

---
+++ liste-repertoire-backend/src/server.js
@@ -0,0 +1,140 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient, ObjectID } from 'mongodb';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const utiliserDB = async (operations, reponse) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true});
+        const db = client.db('liste-repertoire');
+
+        await operations(db);
+
+        client.close();
+    }
+    catch(erreur) {
+        reponse.status(500).send("Erreur de connexion à la bd", erreur);
+    }
+};
+
+app.get('/api/pieces', (requete, reponse) => {
+    utiliserDB(async (db) => {
+        const listePieces = await db.collection('pieces').find().toArray();
+        reponse.status(200).json(listePieces);
+    }, reponse).catch(
+        () => reponse.status(500).send("Erreur lors de la requête")
+    );
+});
+
+app.get('/api/pieces/:id', (requete, reponse) => {
+    const id = requete.params.id;
+
+    utiliserDB(async (db) => {
+        var objectId = ObjectID.createFromHexString(id);
+        const infoPiece = await db.collection('pieces').findOne({ _id: objectId });
+        reponse.status(200).json(infoPiece);      
+    }, reponse).catch(
+        () => reponse.status(500).send("Pièce non trouvée")
+    );
+});
+
+app.put('/api/pieces/ajouter', (requete, reponse) => {
+    const {titre, artiste, categories} = requete.body;
+
+    if (titre !== undefined && artiste !== undefined && categories !== undefined) {
+        utiliserDB(async (db) => {
+            await db.collection('pieces').insertOne({ 
+                titre: titre,
+                artiste: artiste,
+                categories: categories
+            });
+            
+            reponse.status(200).send("Pièce ajoutée");
+        }, reponse).catch(
+            () => reponse.status(500).send("Erreur : la pièce n'a pas été ajoutée")
+        );     
+    }
+    else {
+        reponse.status(500).send(`Certains paramètres ne sont pas définis :
+            - titre: ${titre}
+            - artiste: ${artiste}
+            - categories: ${categories}`);
+    }
+});
+
+app.post('/api/pieces/modifier/:id', (requete, reponse) => {
+    const {titre, artiste, categories} = requete.body;
+    const id = requete.params.id;
+
+    if (titre !== undefined && artiste !== undefined && categories !== undefined) {
+        utiliserDB(async (db) => {
+            var objectId = ObjectID.createFromHexString(id);
+            await db.collection('pieces').updateOne({ _id: objectId }, {
+                '$set': {
+                    titre: titre,
+                    artiste: artiste,
+                    categories: categories
+                }
+            });
+            
+            reponse.status(200).send("Pièce modifiée");
+        }, reponse).catch(
+            () => reponse.status(500).send("Erreur : la pièce n'a pas été modifiée")
+        );        
+    }
+    else {
+        reponse.status(500).send(`Certains paramètres ne sont pas définis :
+            - titre: ${titre}
+            - artiste: ${artiste}
+            - categories: ${categories}`);
+    }
+});
+
+app.delete('/api/pieces/supprimer/:id', (requete, reponse) => {
+    const id = requete.params.id;
+
+    utiliserDB(async (db) => {
+        var objectId = ObjectID.createFromHexString(id);
+        const resultat = await db.collection('pieces').deleteOne({ _id: objectId });
+        
+        reponse.status(200).send(`${resultat.deletedCount} pièce supprimée`);
+    }, reponse).catch(
+        () => reponse.status(500).send("Erreur : la pièce n'a pas été supprimée")
+    );    
+});
+
+app.get('/api/demandes', (requete, reponse) => {
+    utiliserDB(async (db) => {
+        const listeDemandes = await db.collection('demandes').find().toArray();
+        reponse.status(200).json(listeDemandes);
+    }, reponse).catch(
+        () => reponse.status(500).send("Erreur lors de la requête")
+    );
+});
+
+app.put('/api/demandes/ajouter', (requete, reponse) => {
+    const {nom, pieces} = requete.body;
+
+    if (nom !== undefined && pieces !== undefined) {
+        utiliserDB(async (db) => {
+            await db.collection('demandes').insertOne({ 
+                nom: nom,
+                pieces: pieces
+            });
+            
+            reponse.status(200).send("Demande ajoutée");
+        }, reponse).catch(
+            () => reponse.status(500).send("Erreur : la demande n'a pas été ajoutée")
+        );       
+    }
+    else {
+        reponse.status(500).send(`Certains paramètres ne sont pas définis :
+            - nom: ${nom}
+            - pieces: ${pieces}`);
+    }
+});
+
+app.listen(8000, () => console.log("Serveur démarré sur le port 8000"));
---
+++ liste-repertoire-frontend/src/pages/PageAdmin.js
@@ -0,0 +1,43 @@
+import {
+    React,
+    useState,
+    useEffect
+} from 'react';
+
+import ListePiecesAdmin from '../composants/ListePiecesAdmin';
+
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+function PageAdmin() {
+    const [listePieces, setListePieces] = useState([]);
+
+    useEffect(() => {
+        const chercherDonnees = async () => {
+            const resultat = await fetch(`/api/pieces`);
+            const body = await resultat.json().catch((error) => {console.log(error)});
+            setListePieces(body);
+        };
+        chercherDonnees();
+    }, []);
+
+    return (
+        <>
+            <h1>Page administrateur</h1>
+
+            <Link to="/ajouter">
+                <Button>Ajouter une nouvelle pièce</Button>    
+            </Link>
+            
+            <h2>Liste du répertoire</h2>
+            <ListePiecesAdmin pieces={listePieces} />
+
+            <Link to="/liste-demandes">
+                <Button>Voir les demandes spéciales</Button>
+            </Link>
+            
+        </>
+    );
+}
+
+export default PageAdmin;
