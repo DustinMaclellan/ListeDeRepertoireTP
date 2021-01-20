a603429c8830a94f2ef495073f57ce4278d390b7
Zebster05 <jean_02_jean@hotmail.com>
2021-01-20 18:44:22 -0500

    style: ajout d'un bouton deconnecte
    Changement de couleur du bouton et du redirect lorsque connecte

--- liste-repertoire-frontend/src/composants/BarreNavigation.js
+++ liste-repertoire-frontend/src/composants/BarreNavigation.js
@@ -1,35 +1,52 @@
-import React from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-import {Navbar,Button} from 'react-bootstrap';
-import Nav from 'react-bootstrap/Nav';
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Button } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import { UtiliserAuth } from "../context/auth";
 
 function BarreNavigation() {
-    return (
-        <Navbar className="navbar-dark bg-dark" >
-        <Navbar.Brand href="/">Maison</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <LinkContainer to="/" exact>
-                        <Nav.Link>Accueil</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/repertoire">
-                        <Nav.Link>Repertoire</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/demande-speciale">
-                        <Nav.Link>Demande spéciale</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/admin">
-                        <Nav.Link>Admin</Nav.Link>
-                    </LinkContainer>                   
-                </Nav>
-                <LinkContainer to="/connexion">
-                <Button className="btn btn-success float-right">Connexion</Button> 
-                </LinkContainer>
-            </Navbar.Collapse>
-        </Navbar>
-        
-    );
+  const { authentification } = UtiliserAuth();
+
+  const handleClick = () => {
+    authentification(false);
+  };
+
+  return (
+    <Navbar className="navbar-dark bg-dark">
+      <Navbar.Brand href="/">Maison</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="/" exact>
+            <Nav.Link>Accueil</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/repertoire">
+            <Nav.Link>Repertoire</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/demande-speciale">
+            <Nav.Link>Demande spéciale</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/admin">
+            <Nav.Link>Admin</Nav.Link>
+          </LinkContainer>
+        </Nav>
+        {authentification == true ? (
+          <LinkContainer to="/connexion">
+            <Button
+              className="btn btn-danger float-right"
+              onClick={handleClick}
+            >
+              Deconnexion
+            </Button>
+          </LinkContainer>
+        ) : (
+          <LinkContainer to="/connexion">
+            <Button className="btn btn-success float-right">Connexion</Button>
+          </LinkContainer>
+        )}
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default BarreNavigation;
