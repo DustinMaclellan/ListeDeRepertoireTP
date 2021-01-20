e09f8868047a09944c524ff3dd6ec3692693dd5c
Zebster05 <jean_02_jean@hotmail.com>
2021-01-20 15:13:33 -0500

    feat: creation page pour ajout utilisateur

---
+++ liste-repertoire-frontend/src/pages/PageAjoutUtilisateur.js
@@ -0,0 +1,10 @@
+import react from 'react'
+import FormulaireNouvelUtilisateur from '../composants/FormulaireNouvelUtilisateur';
+
+function PageAjoutUtlisateur() {
+    return (
+        <FormulaireNouvelUtilisateur />   
+    )
+}
+
+export default PageAjoutUtlisateur;
