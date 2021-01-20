873183aec2e84b3af440d4764198ad8b61cfd7a8
Zebster05 <jean_02_jean@hotmail.com>
2021-01-20 12:41:28 -0500

    feat: creation page connexion
    Page connexion cree avec le formulaire Login

---
+++ liste-repertoire-frontend/src/pages/PageConnexion.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormulaireLogin from '../composants/FormulaireConnexion'
+
+function PageConnexion() {
+
+    return(
+        <>
+        <FormulaireLogin/>
+        </>
+    )
+
+}
+
+export default PageConnexion;
