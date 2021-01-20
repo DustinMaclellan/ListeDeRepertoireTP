43340c0aede5da8b42d5232414f78a129cd29c8e
Zebster05 <jean_02_jean@hotmail.com>
2021-01-20 15:11:15 -0500

    fix: correction de la maniere de login
    Changement pour verification du login au lieu de creer un utilisateur

--- liste-repertoire-frontend/src/composants/FormulaireConnexion.js
+++ liste-repertoire-frontend/src/composants/FormulaireConnexion.js
@@ -1,41 +1,76 @@
-import {react, useState} from 'react';
-import {Form, Button, Card} from 'react-bootstrap'
+import { react, useState } from "react";
+import { Form, Button, Card } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
 
+function FormulaireLogin() {
+  const [nomUtilisateur, setNomUtilisateur] = useState();
+  const [motDePasse, setMotDePasse] = useState();
+  const [rediriger, setRediriger] = useState();
 
-function FormulaireLogin(){
+  const validationBaseDeDonnees = async () => {
+    const resultat = await fetch(`/api/utilisateur/verification`, {
+        method: 'post',
+        body: JSON.stringify({ nomUtilisateur,motDePasse }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    
+    const body = await resultat.json().catch((error) => {
+      console.log(error);
+    });
 
-    const [nomUtilisateur, setNomUtilisateur] = useState();
-    const [motDePasse, setMotDePasse] = useState();
+    if (body == "true") {
+      setRediriger(true);
+    } else {
+      alert("Il n'existe pas de login");
+    }
+  };
 
-const envoyerFormulaire = async () => {
-       
-        await fetch(`/api/utilisateur/ajouter`, {
-            method: 'post',
-            body: JSON.stringify({nomUtilisateur,motDePasse}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-    };
+  function afficherRedirection() {
+    if (rediriger === true && nomUtilisateur == "admin") {
+      return <Redirect to="/admin"></Redirect>;
+    }
 
-    return (
+    if (rediriger === true && nomUtilisateur !== "admin") {
+      return <Redirect to="/connexion" />;
+    }
+  }
 
-         <Form className="card mt-5">
-                <Card.Header as="h4">Connexion</Card.Header>
-                <Form.Group className="p-2">
-                <Form.Label>Nom Utilisateur</Form.Label>
-                <Form.Control type="text" value={nomUtilisateur} 
-                    onChange={(event) => setNomUtilisateur(event.target.value)} />
-                </Form.Group>
-                <Form.Group className="p-2">
-                <Form.Label>Mot de passe</Form.Label>
-                <Form.Control type="password" value={motDePasse} 
-                    onChange={(event) => setMotDePasse(event.target.value)} />
-                </Form.Group>
-                <Button variant="outline-success" size="lg" onClick={envoyerFormulaire}>Se connecter</Button>
-                <Button className="mt-2" variant="outline-warning" size="lg" to="/">Vous n'avez pas de compte ?</Button>             
-            </Form>
-    )
+  return (
+    <Form className="card mt-5">
+      <Card.Header as="h4">Connexion</Card.Header>
+      <Form.Group className="p-2">
+        <Form.Label>Nom Utilisateur</Form.Label>
+        <Form.Control
+          type="text"
+          value={nomUtilisateur}
+          onChange={(event) => setNomUtilisateur(event.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="p-2">
+        <Form.Label>Mot de passe</Form.Label>
+        <Form.Control
+          type="password"
+          value={motDePasse}
+          onChange={(event) => setMotDePasse(event.target.value)}
+        />
+      </Form.Group>
+      <Button
+        variant="outline-success"
+        size="lg"
+        onClick={validationBaseDeDonnees}
+      >
+        Se connecter
+      </Button>
+      <a href="/creerUtilisateur">
+        <Button className="mt-2" variant="outline-warning" size="lg">
+          Vous n'avez pas de compte ?
+        </Button>
+      </a>
+      {afficherRedirection()}
+    </Form>
+  );
 }
 
 export default FormulaireLogin;
