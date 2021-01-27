f140453f6a63df758764d53f7d35ee5c2c0e6e9a
Keven <keven.br2000@gmail.com>
2021-01-27 13:38:32 -0500

    feat: Ajout des tries dans la liste des pièces
    Changement de l'utilisation d'une liste par un tableau dans l'affichage des pièces du répertoire et ajout de boutos afin de trier par ordre alphabétique les champs du répertoire.

--- liste-repertoire-frontend/src/composants/ListePieces.js
+++ liste-repertoire-frontend/src/composants/ListePieces.js
@@ -1,69 +1,178 @@
-import React from 'react';
-import Alert from 'react-bootstrap/Alert'
+import React from "react";
+import Table from "react-bootstrap/Table";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
 
-function ListePieces({ pieces, handleClick, listeDemandes }) {
-    if (pieces?.length) {
-        var dictionnaireCategories = Object();
+function ListePieces({ pieces, handleClick, listeDemandes, setListePieces }) {
+  if (pieces?.length) {
+    if (listeDemandes !== undefined) {
+      var listeIdDemandes = Object.keys(listeDemandes);
+    }
 
-        pieces.forEach(piece => {
-            piece.categories.forEach(categorie => {
-                if (dictionnaireCategories[categorie] === undefined) {
-                    dictionnaireCategories[categorie] = true;
+    function TrierAsc(champ) {
+      return function (a, b) {
+        if (a[champ] > b[champ]) {
+          return 1;
+        } else if (a[champ] < b[champ]) {
+          return -1;
+        }
+        return 0;
+      };
+    }
+    function TrierDesc(champ) {
+      return function (a, b) {
+        if (a[champ] > b[champ]) {
+          return -1;
+        } else if (a[champ] < b[champ]) {
+          return 1;
+        }
+        return 0;
+      };
+    }
+
+    function TrierChamp(champ, direction) {
+      var listeTemp = [...pieces];
+
+      if (direction === "asc") {
+        listeTemp.sort(TrierAsc(champ));
+      } else {
+        listeTemp.sort(TrierDesc(champ));
+      }
+
+      setListePieces(listeTemp);
+    }
+
+    return (
+      <>
+        <Table>
+          <thead>
+            <th>
+              Titre{" "}
+              <Button
+                size="sm"
+                onClick={() => {
+                  TrierChamp("titre", "asc");
+                }}
+              >
+                ▲
+              </Button>
+              <Button
+                size="sm"
+                onClick={() => {
+                  TrierChamp("titre", "desc");
+                }}
+              >
+                ▼
+              </Button>
+            </th>
+            <th>
+              Artiste{" "}
+              <Button
+                size="sm"
+                onClick={() => {
+                  TrierChamp("artiste", "asc");
+                }}
+              >
+                ▲
+              </Button>
+              <Button
+                size="sm"
+                onClick={() => {
+                  TrierChamp("artiste", "desc");
+                }}
+              >
+                ▼
+              </Button>
+            </th>
+            <th>
+              Catégories{" "}
+              <Button
+                size="sm"
+                onClick={() => {
+                  TrierChamp("categories", "asc");
+                }}
+              >
+                ▲
+              </Button>
+              <Button
+                size="sm"
+                onClick={() => {
+                  TrierChamp("categories", "desc");
+                }}
+              >
+                ▼
+              </Button>
+            </th>
+          </thead>
+          <tbody>
+            {pieces.map((piece) => {
+              if (handleClick !== undefined) {
+                if (listeIdDemandes.includes(piece._id)) {
+                  console.log(1);
+                  return (
+                    <tr
+                      key={piece._id}
+                      onClick={() => handleClick(piece._id)}
+                      className="bg-info"
+                    >
+                      <td>{piece.titre}</td>
+                      <td>{piece.artiste}</td>
+                      <td>
+                        {piece.categories.map((categorie, index) => {
+                          if (index === piece.categories.length - 1) {
+                            return <>{categorie}</>;
+                          } else {
+                            return <>{categorie} - </>;
+                          }
+                        })}
+                      </td>
+                    </tr>
+                  );
+                } else {
+                  console.log(2);
+                  return (
+                    <tr key={piece._id} onClick={() => handleClick(piece._id)}>
+                      <td>{piece.titre}</td>
+                      <td>{piece.artiste}</td>
+                      <td>
+                        {piece.categories.map((categorie, index) => {
+                          if (index === piece.categories.length - 1) {
+                            return <>{categorie}</>;
+                          } else {
+                            return <>{categorie} - </>;
+                          }
+                        })}
+                      </td>
+                    </tr>
+                  );
                 }
-            })            
-        });
-
-        const categories = Object.keys(dictionnaireCategories);
-
-        if (listeDemandes !== undefined) {
-            var listeIdDemandes = Object.keys(listeDemandes);
-        }        
-
-        return (
-            <>
-                {categories.map((categorie) => {
-                    const piecesAssociees = pieces.filter((piece) => 
-                        piece.categories.indexOf(categorie) !== -1);
-
-                    return (
-                        <div key={categorie}>
-                            <h4>{categorie}</h4>
-                            <ul>
-                                {
-                                    piecesAssociees.map(piece => {
-                                        if (handleClick !== undefined) {
-                                            if (listeIdDemandes.includes(piece._id)) {
-                                                console.log(1)
-                                                return <li key={piece._id} onClick={() => handleClick(piece._id)} className="bg-info">
-                                                            {piece.titre} - {piece.artiste}
-                                                        </li>
-                                            }
-                                            else {
-                                                console.log(2)
-                                                return <li key={piece._id} onClick={() => handleClick(piece._id)} >
-                                                            {piece.titre} - {piece.artiste}
-                                                        </li>
-                                            }
-                                            
-                                        }
-                                        else {
-                                            return <li key={piece._id} >
-                                                        {piece.titre} - {piece.artiste}
-                                                    </li>
-                                        }
-                                    })
-                                        
-                                }
-                            </ul>
-                        </div>
-                    )
-                })}
-            </>
-        );
-    }
-    else {
-        return <Alert variant={"info"} >Il n'y a pas de pièces dans le répertoire.</Alert>;
-    }
+              } else {
+                return (
+                  <tr>
+                    <td>{piece.titre}</td>
+                    <td>{piece.artiste}</td>
+                    <td>
+                      {piece.categories.map((categorie, index) => {
+                        if (index === piece.categories.length - 1) {
+                          return <>{categorie}</>;
+                        } else {
+                          return <>{categorie} - </>;
+                        }
+                      })}
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </Table>
+      </>
+    );
+  } else {
+    return (
+      <Alert variant={"info"}>Il n'y a pas de pièces dans le répertoire.</Alert>
+    );
+  }
 }
 
 export default ListePieces;
