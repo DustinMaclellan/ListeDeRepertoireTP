a866f046b542d4b10776b957a463662b5fd1af10
Keven <keven.br2000@gmail.com>
2021-01-31 19:15:40 -0500

    feat: Ajout paramètre SetListePiece
    Ajout du paramètre setListePiecces dans l'appel du composant ListePiecesAdmin afin de supporter le tries des champs.

--- liste-repertoire-frontend/src/pages/PageAdmin.js
+++ liste-repertoire-frontend/src/pages/PageAdmin.js
@@ -1,43 +1,40 @@
-import {
-    React,
-    useState,
-    useEffect
-} from 'react';
+import { React, useState, useEffect } from "react";
 
-import ListePiecesAdmin from '../composants/ListePiecesAdmin';
+import ListePiecesAdmin from "../composants/ListePiecesAdmin";
 
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 function PageAdmin() {
-    const [listePieces, setListePieces] = useState([]);
+  const [listePieces, setListePieces] = useState([]);
 
-    useEffect(() => {
-        const chercherDonnees = async () => {
-            const resultat = await fetch(`/api/pieces`);
-            const body = await resultat.json().catch((error) => {console.log(error)});
-            setListePieces(body);
-        };
-        chercherDonnees();
-    }, []);
+  useEffect(() => {
+    const chercherDonnees = async () => {
+      const resultat = await fetch(`/api/pieces`);
+      const body = await resultat.json().catch((error) => {
+        console.log(error);
+      });
+      setListePieces(body);
+    };
+    chercherDonnees();
+  }, []);
 
-    return (
-        <>
-            <h1>Page administrateur</h1>
+  return (
+    <>
+      <h1>Page administrateur</h1>
 
-            <Link to="/ajouter">
-                <Button>Ajouter une nouvelle pièce</Button>    
-            </Link>
-            
-            <h2>Liste du répertoire</h2>
-            <ListePiecesAdmin pieces={listePieces} />
+      <Link to="/ajouter">
+        <Button>Ajouter une nouvelle pièce</Button>
+      </Link>
 
-            <Link to="/liste-demandes">
-                <Button>Voir les demandes spéciales</Button>
-            </Link>
-            
-        </>
-    );
+      <h2>Liste du répertoire</h2>
+      <ListePiecesAdmin pieces={listePieces} setListePieces={setListePieces} />
+
+      <Link to="/liste-demandes">
+        <Button>Voir les demandes spéciales</Button>
+      </Link>
+    </>
+  );
 }
 
 export default PageAdmin;
