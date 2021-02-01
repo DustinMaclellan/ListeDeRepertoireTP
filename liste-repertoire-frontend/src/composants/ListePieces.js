6502b5bc545db51396b252f47a2be6411b9d8c8e
Keven <keven.br2000@gmail.com>
2021-01-31 21:18:01 -0500

    feat: Ajout des Filtres sur ListePiece
    Ajout de l'implémentation des filtres sur les champs du tableau dans le composant ListePiece

--- liste-repertoire-frontend/src/composants/ListePieces.js
+++ liste-repertoire-frontend/src/composants/ListePieces.js
@@ -3,8 +3,14 @@ import Table from "react-bootstrap/Table";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 
-
-function ListePieces({ pieces, handleClick, listeDemandes, setListePieces }) {
+function ListePieces({
+  pieces,
+  handleClick,
+  listeDemandes,
+  setListePieces,
+  demandePieces,
+  setDemandePieces,
+}) {
   const [rechercheCat, setRechercheCat] = useState("");
   const [rechercheArtiste, setRechercheArtiste] = useState("");
   const [rechercheTitre, setRechercheTitre] = useState("");
@@ -13,186 +19,118 @@ function ListePieces({ pieces, handleClick, listeDemandes, setListePieces }) {
   const [directionTrieArtiste, setDirectionTrieArtiste] = useState("asc");
   const [directionTrieCategorie, setDirectionTrieCategorie] = useState("asc");
 
-  var listeTemp = [];
+  useEffect(() => {
+    FiltrerChamp();
+  }, [rechercheTitre, rechercheArtiste, rechercheCat]);
+
   console.log(pieces);
 
-  useEffect(() => {
-    pieces.forEach((element) => {
-      if (element.artiste.includes(rechercheArtiste)) {
-        if (element.titre.includes(rechercheTitre)) {
-          if (
-            Object.values(element.categories).filter((w) =>
-              w.includes(rechercheCat)
-            )
-          ) {
-            listeTemp.push(element);
-          }
+  function TrierAsc(champ) {
+    return function (a, b) {
+      if (a[champ] > b[champ]) {
+        return 1;
+      } else if (a[champ] < b[champ]) {
+        return -1;
+      }
+      return 0;
+    };
+  }
+  function TrierDesc(champ) {
+    return function (a, b) {
+      if (a[champ] > b[champ]) {
+        return -1;
+      } else if (a[champ] < b[champ]) {
+        return 1;
+      }
+      return 0;
+    };
+  }
+
+  function TrierChamp(champ, direction) {
+    var listeTemp = [...pieces];
+
+    if (champ === "categories") {
+      listeTemp.forEach((piece) => {
+        if (direction === "asc") {
+          piece.categories.sort();
+        } else {
+          piece.categories.reverse();
+        }
+      });
+    }
+
+    if (direction === "asc") {
+      listeTemp.sort(TrierAsc(champ));
+    } else {
+      listeTemp.sort(TrierDesc(champ));
+    }
+
+    setListePieces(listeTemp);
+  }
+
+  function FiltrerChamp() {
+    var listeTemp = [];
+
+    if (rechercheArtiste == "" && rechercheTitre == "" && rechercheCat == "") {
+      setDemandePieces(!demandePieces);
+    }
+
+    pieces.forEach((piece) => {
+      var titreLowerCase = piece.titre.toLowerCase();
+
+      if (titreLowerCase.includes(rechercheTitre.toLowerCase())) {
+        var artisteLowerCase = piece.artiste.toLowerCase();
+
+        if (artisteLowerCase.includes(rechercheArtiste.toLowerCase())) {
+          piece.categories.forEach((categorie) => {
+            var categorieLowerCase = categorie.toLowerCase();
+
+            if (categorieLowerCase.includes(rechercheCat.toLowerCase())) {
+              listeTemp.push(piece);
+            }
+          });
         }
       }
     });
 
-    setListePieces(listeTemp);
-  }, [rechercheTitre, rechercheArtiste, rechercheCat]);
+    setListePieces([...new Set(listeTemp)]);
+  }
 
-  // if (pieces?.length) {
-    if (listeDemandes !== undefined) {
-      var listeIdDemandes = Object.keys(listeDemandes);
-    }
+  function SupprimerFiltres() {
+    setRechercheTitre("");
+    setRechercheArtiste("");
+    setRechercheCat("");
+  }
 
-    function TrierAsc(champ) {
-      return function (a, b) {
-        if (a[champ] > b[champ]) {
-          return 1;
-        } else if (a[champ] < b[champ]) {
-          return -1;
-        }
-        return 0;
-      };
-    }
-    function TrierDesc(champ) {
-      return function (a, b) {
-        if (a[champ] > b[champ]) {
-          return -1;
-        } else if (a[champ] < b[champ]) {
-          return 1;
-        }
-        return 0;
-      };
-    }
-
-    function TrierChamp(champ, direction) {
-      var listeTemp = [...pieces];
-
-      if (champ === "categories") {
-        listeTemp.forEach((piece) => {
-          if (direction === "asc") {
-            piece.categories.sort();
-          } else {
-            piece.categories.reverse();
-          }
-        });
-      }
-
-      if (direction === "asc") {
-        listeTemp.sort(TrierAsc(champ));
-      } else {
-        listeTemp.sort(TrierDesc(champ));
-      }
-
-      setListePieces(listeTemp);
-    }
-
-    return (
-      <>
-        <Table>
-          <thead>
-            <th>
-              Titre{" "}
-              <Button
-                size="sm"
-                onClick={() => {
-                  setDirectionTrieTitre(
-                    directionTrieTitre === "asc" ? "desc" : "asc"
-                  );
-                  TrierChamp("titre", directionTrieTitre);
-                }}
-              >
-                {directionTrieTitre === "asc" ? "▼" : "▲"}
-              </Button>
-              <br />
-              <input
-              type="text"
-              placeholder="filtre par titre"
-              value={rechercheTitre}
-              onChange={(e) => setRechercheTitre(e.target.value)}
-            />
-            </th>
-            <th>
-              Artiste{" "}
-              <Button
-                size="sm"
-                onClick={() => {
-                  setDirectionTrieArtiste(
-                    directionTrieArtiste === "asc" ? "desc" : "asc"
-                  );
-                  TrierChamp("artiste", directionTrieArtiste);
-                }}
-              >
-                {directionTrieArtiste === "asc" ? "▼" : "▲"}
-              </Button>
-              <br />
-              <input
-              type="text"
-              placeholder="filtre par artiste"
-              value={rechercheArtiste}
-              onChange={(e) => setRechercheArtiste(e.target.value)}
-            />
-            </th>
-            <th>
-              Catégories{" "}
-              <Button
-                size="sm"
-                onClick={() => {
-                  setDirectionTrieCategorie(
-                    directionTrieCategorie === "asc" ? "desc" : "asc"
-                  );
-                  TrierChamp("categories", directionTrieCategorie);
-                }}
-              >
-                {directionTrieCategorie === "asc" ? "▼" : "▲"}
-              </Button>
-              <br />
-              <input
-              type="text"
-              placeholder="filtre par catégorie"
-              value={rechercheCat}
-              onChange={(e) => setRechercheCat(e.target.value)}
-            />
-            </th>
-          </thead>
-          <tbody>
-            {pieces.map((piece) => {
-              if (handleClick !== undefined) {
-                if (listeIdDemandes.includes(piece._id)) {
-                  return (
-                    <tr
-                      key={piece._id}
-                      onClick={() => handleClick(piece._id)}
-                      className="bg-info"
-                    >
-                      <td>{piece.titre}</td>
-                      <td>{piece.artiste}</td>
-                      <td>
-                        {piece.categories.map((categorie, index) => {
-                          if (index === piece.categories.length - 1) {
-                            return <>{categorie}</>;
-                          } else {
-                            return <>{categorie} - </>;
-                          }
-                        })}
-                      </td>
-                    </tr>
-                  );
-                } else {
-                  return (
-                    <tr key={piece._id} onClick={() => handleClick(piece._id)}>
-                      <td>{piece.titre}</td>
-                      <td>{piece.artiste}</td>
-                      <td>
-                        {piece.categories.map((categorie, index) => {
-                          if (index === piece.categories.length - 1) {
-                            return <>{categorie}</>;
-                          } else {
-                            return <>{categorie} - </>;
-                          }
-                        })}
-                      </td>
-                    </tr>
-                  );
-                }
+  function RenduBodyTable() {
+    if (pieces?.length) {
+      return (
+        <tbody>
+          {pieces.map((piece) => {
+            if (handleClick !== undefined) {
+              if (listeIdDemandes.includes(piece._id)) {
+                return (
+                  <tr
+                    key={piece._id}
+                    onClick={() => handleClick(piece._id)}
+                    className="bg-info"
+                  >
+                    <td>{piece.titre}</td>
+                    <td>{piece.artiste}</td>
+                    <td>
+                      {piece.categories.map((categorie, index) => {
+                        if (index === piece.categories.length - 1) {
+                          return <>{categorie}</>;
+                        } else {
+                          return <>{categorie} - </>;
+                        }
+                      })}
+                    </td>
+                  </tr>
+                );
               } else {
                 return (
-                  <tr>
+                  <tr key={piece._id} onClick={() => handleClick(piece._id)}>
                     <td>{piece.titre}</td>
                     <td>{piece.artiste}</td>
                     <td>
@@ -207,16 +145,148 @@ function ListePieces({ pieces, handleClick, listeDemandes, setListePieces }) {
                   </tr>
                 );
               }
-            })}
-          </tbody>
-        </Table>
-      </>
-    );
-  // } else {
-  //   return (
-  //     <Alert variant={"info"}>Il n'y a pas de pièces dans le répertoire.</Alert>
-  //   );
-  // }
+            } else {
+              return (
+                <tr>
+                  <td>{piece.titre}</td>
+                  <td>{piece.artiste}</td>
+                  <td>
+                    {piece.categories.map((categorie, index) => {
+                      if (index === piece.categories.length - 1) {
+                        return <>{categorie}</>;
+                      } else {
+                        return <>{categorie} - </>;
+                      }
+                    })}
+                  </td>
+                </tr>
+              );
+            }
+          })}
+        </tbody>
+      );
+    }
+  }
+
+  function RenduRepertoireVide() {
+    if (!pieces?.length) {
+      return (
+        <Alert variant={"info"}>
+          Il n'y a pas de pièces dans le répertoire.
+        </Alert>
+      );
+    }
+  }
+
+  if (listeDemandes !== undefined) {
+    var listeIdDemandes = Object.keys(listeDemandes);
+  }
+
+  return (
+    <>
+      <Table>
+        <thead>
+          <th>
+            Titre{" "}
+            <Button
+              size="sm"
+              onClick={() => {
+                setDirectionTrieTitre(
+                  directionTrieTitre === "asc" ? "desc" : "asc"
+                );
+                TrierChamp("titre", directionTrieTitre);
+              }}
+            >
+              {directionTrieTitre === "asc" ? "▼" : "▲"}
+            </Button>
+            <br />
+            <input
+              type="text"
+              placeholder="filtre par titre"
+              value={rechercheTitre}
+              onChange={(e) => setRechercheTitre(e.target.value)}
+            />
+            <Button
+              size="sm"
+              variant="danger"
+              onClick={() => {
+                setRechercheTitre("");
+              }}
+            >
+              Clear
+            </Button>
+          </th>
+          <th>
+            Artiste{" "}
+            <Button
+              size="sm"
+              onClick={() => {
+                setDirectionTrieArtiste(
+                  directionTrieArtiste === "asc" ? "desc" : "asc"
+                );
+                TrierChamp("artiste", directionTrieArtiste);
+              }}
+            >
+              {directionTrieArtiste === "asc" ? "▼" : "▲"}
+            </Button>
+            <br />
+            <input
+              type="text"
+              placeholder="filtre par artiste"
+              value={rechercheArtiste}
+              onChange={(e) => setRechercheArtiste(e.target.value)}
+            />
+            <Button
+              size="sm"
+              variant="danger"
+              onClick={() => {
+                setRechercheArtiste("");
+              }}
+            >
+              Clear
+            </Button>
+          </th>
+          <th>
+            Catégories{" "}
+            <Button
+              size="sm"
+              onClick={() => {
+                setDirectionTrieCategorie(
+                  directionTrieCategorie === "asc" ? "desc" : "asc"
+                );
+                TrierChamp("categories", directionTrieCategorie);
+              }}
+            >
+              {directionTrieCategorie === "asc" ? "▼" : "▲"}
+            </Button>
+            <br />
+            <input
+              type="text"
+              placeholder="filtre par catégorie"
+              value={rechercheCat}
+              onChange={(e) => setRechercheCat(e.target.value)}
+            />
+            <Button
+              size="sm"
+              variant="danger"
+              onClick={() => {
+                setRechercheCat("");
+              }}
+            >
+              Clear
+            </Button>
+          </th>
+        </thead>
+        {RenduBodyTable()}
+      </Table>
+
+      {RenduRepertoireVide()}
+
+      <Button variant="danger" onClick={() => SupprimerFiltres()}>
+        Supprimer Filtres
+      </Button>
+    </>
+  );
 }
 
 export default ListePieces;
