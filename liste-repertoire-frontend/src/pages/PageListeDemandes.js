178947edf4cc29655ce2284b4749b8475abc26d4
Keven <keven.br2000@gmail.com>
2021-02-03 14:00:06 -0500

    fix: Utilisation du composant ListeDemandesSpeciales
    Utilisation du composant ListeDemandesSpeciales afin d'afficher la liste des demandes du côté de l'administrateur.

--- liste-repertoire-frontend/src/pages/PageListeDemandes.js
+++ liste-repertoire-frontend/src/pages/PageListeDemandes.js
@@ -1,8 +1,6 @@
 import { React, useState, useEffect } from "react";
 
-import ListGroup from "react-bootstrap/ListGroup";
-import Button from "react-bootstrap/Button";
-import Table from "react-bootstrap/Table";
+import ListeDemandesSpeciales from "../composants/ListeDemandesSpeciales";
 
 function PageListeDemandes() {
   const [listeDemandes, setListeDemandes] = useState({});
@@ -35,68 +33,11 @@ function PageListeDemandes() {
 
   return (
     <>
-      <h1>Demandes spéciales</h1>
-      <ListGroup>
-        {Object.keys(listeDemandes).map((keys) => (
-          <ListGroup.Item
-            className={listeDemandes[keys].actif === 1 ? "d-block" : "d-none"}
-          >
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Nom Client</th>
-                  <th>Pieces</th>
-                  <th>Modifier Etat</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <h4>{listeDemandes[keys].nomClient}</h4>
-                  </td>
-                  <td>
-                    {Object.keys(listeDemandes[keys].pieces).map((chanson) => (
-                      <ul>
-                        <li>
-                          <b>Titre:</b>{" "}
-                          {listeDemandes[keys].pieces[chanson].titre}
-                        </li>
-                        <li>
-                          <b>Artiste: </b>
-                          {listeDemandes[keys].pieces[chanson].artiste}
-                        </li>
-                        <li>
-                          <b>Categories:</b>{" "}
-                        </li>
-                        <ol>
-                          {listeDemandes[keys].pieces[chanson].categories.map(
-                            (element) => (
-                              <li>{element}</li>
-                            )
-                          )}
-                        </ol>
-                      </ul>
-                    ))}
-                  </td>
-                  <td>
-                    {" "}
-                    {listeDemandes[keys].actif === 1 ? (
-                      <Button
-                        variant="warning"
-                        onClick={() => handleClick(listeDemandes[keys]._id)}
-                      >
-                        Rendre inactive
-                      </Button>
-                    ) : (
-                      <h4>Demande inactive</h4>
-                    )}
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+      <ListeDemandesSpeciales
+        listeDemandes={listeDemandes}
+        setListeDemandes = {setListeDemandes}
+        handleClick={handleClick}
+      />
     </>
   );
 }
